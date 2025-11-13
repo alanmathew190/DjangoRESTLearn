b8483cf22ee6fdfb3a77758be44a35426d7c0e21
Alan <alanmathew190@gmail.com>
2025-11-13 12:12:49 +0530

    push day 2

--- NotesAppLearning/Notes/views.py
+++ NotesAppLearning/Notes/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import JsonResponse
+from .models import NoteInfo
+from .serializers import NoteSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
+@api_view(['GET','POST'])
 def print_hello(request):
-    return HttpResponse("Hello World")
+    if request.method=='GET':
+        notes=NoteInfo.objects.all()
+        serializers=NoteSerializer(notes,many=True)
+        return JsonResponse(serializers.data)
+    
+    elif request.method=='POST':
+        serializers=NoteSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
